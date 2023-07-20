e2066a641569944b1fc0915f4c6ff4ba6e196bc4
Chris J Mears <chris.mears@gmail.com>
2023-07-20 01:14:06 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
+<p class="mt-3">
+	Visit <a class="underline hover:text-red-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to read
+	the documentation
+</p>
