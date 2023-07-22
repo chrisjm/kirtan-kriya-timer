73e253ef6e0342e7b2a4afbb9610c387749ec62e
Chris J Mears <chris.mears@gmail.com>
2023-07-22 00:33:03 +0000

    Add tones; working timer

--- src/lib/components/TimerDisplay.svelte
+++ src/lib/components/TimerDisplay.svelte
@@ -15,16 +15,31 @@
 		minutesToMilliseconds(2),
 		minutesToMilliseconds(2)
 	];
+	let actions = ['Chanting', 'Whispering', 'Thinking', 'Whispering', 'Chanting'];
 	let duration: number | undefined;
 	let timerEndTime: number;
 	let timerId: NodeJS.Timer;
 	let timerIndex = 0;
+	let actionLabel = '';
 	let timeRemaining: number = durations[0];
 	let mantra = '';
+	let synth: Tone.PolySynth<Tone.Synth<Tone.SynthOptions>>;
+	let loop: Tone.Loop<Tone.LoopOptions>;
+	let noteIndex = 0;
+	let soundLoaded = false;
+
+	$: actionLabel = actions[timerIndex];
+
+	const notes = [
+		{ pitch: 'E4', mantra: 'Sa' },
+		{ pitch: 'D4', mantra: 'Ta' },
+		{ pitch: 'C4', mantra: 'Na' },
+		{ pitch: 'D4', mantra: 'Ma' }
+	];
 
 	function updateCountdown() {
-		let now = Date.now();
 		clearTimeout(timerId);
+		let now = Date.now();
 		timeRemaining = timerEndTime - now;
 
 		if (timeRemaining > 0) {
@@ -42,65 +57,75 @@
 			timerEndTime = Date.now() + duration;
 			updateCountdown();
 		} else {
-			cancelTimers();
+			pauseTimers();
 		}
 	}
 
-	async function startTimers() {
-		timerIndex = 0;
-		duration = durations[timerIndex];
-		timerEndTime = Date.now() + duration;
-		isRunning = true;
-		updateCountdown();
-		await Tone.start();
-		const synth = new Tone.PolySynth().toDestination();
-		const notes = [
-			{ pitch: 'E4', mantra: 'SA' },
-			{ pitch: 'D4', mantra: 'TA' },
-			{ pitch: 'C4', mantra: 'NA' },
-			{ pitch: 'D4', mantra: 'MA' }
-		];
-		let i = 0;
-		const loopA = new Tone.Loop((time) => {
-			synth.triggerAttackRelease(notes[i].pitch, '4n', time);
-      mantra = notes[i].mantra;
-			i += 1;
-			if (i >= notes.length) {
-				i = 0;
-			}
-		}, '4n').start(0);
+	function startTones() {
 		Tone.Transport.bpm.value = 60;
 		Tone.Transport.start();
 	}
 
-	function formatTime(time: number) {
-		const seconds = millisecondsToSeconds(time);
-		const minutes = secondsToMinutes(seconds);
-		const remainingSeconds = seconds % 60;
-		return `${padWithZeroes(minutes)}:${padWithZeroes(remainingSeconds)}`;
+	async function initializeSound() {
+		await Tone.start();
+		synth = new Tone.PolySynth().toDestination();
+		loop = new Tone.Loop((time) => {
+			synth.triggerAttackRelease(notes[noteIndex].pitch, '8n', time);
+			mantra = notes[noteIndex].mantra;
+			noteIndex += 1;
+			if (noteIndex >= notes.length) {
+				noteIndex = 0;
+			}
+		}).start(0);
+		soundLoaded = true;
 	}
 
-	function cancelTimers() {
-		// TODO: Add some logic to prompt the user to record the cause of the interruption.
-		idle();
+	function resetTimers() {
+		timerIndex = 0;
+		noteIndex = 0;
+		duration = durations[timerIndex];
+		timeRemaining = duration;
 	}
 
-	function idle() {
-		isRunning = false;
-		Tone.Transport.stop();
+	async function startTimers() {
+		isRunning = true;
+		if (!duration) {
+			duration = durations[0];
+		}
+		timerEndTime = Date.now() + timeRemaining;
+		updateCountdown();
+		if (!soundLoaded) {
+			initializeSound();
+		}
+		startTones();
+	}
+
+	function pauseTimers() {
 		clearTimeout(timerId);
+		isRunning = false;
+		Tone.Transport.pause();
 	}
 </script>
 
-<section>
-	<p>
-		{formatTime(timeRemaining)}
-	</p>
-	<p>
-		{mantra}
-	</p>
-	<footer>
-		<button on:click={startTimers} disabled={isRunning}>start</button>
-		<button on:click={cancelTimers} disabled={!isRunning}>cancel</button>
-	</footer>
+<section class="text-center my-6">
+	<span class="countdown font-mono text-3xl">
+		<span
+			style="--value:{padWithZeroes(secondsToMinutes(millisecondsToSeconds(timeRemaining)))};"
+		/>:
+		<span style="--value:{padWithZeroes(millisecondsToSeconds(timeRemaining) % 60)};" />
+	</span>
+	<div class="my-3">
+		{#if isRunning}
+			<button class="btn btn-warning" on:click={pauseTimers} disabled={!isRunning}>PAUSE</button>
+		{:else}
+			<button class="btn btn-success" on:click={startTimers} disabled={isRunning}>START</button>
+		{/if}
+		<button class="btn btn-error" on:click={resetTimers} disabled={isRunning}>RESET</button>
+	</div>
+	{#if isRunning}
+		<p class="my-3">
+			<span class="">{actionLabel}:</span>
+			<span class="bold">{mantra}</span>
+		</p>
+	{/if}
 </section>
