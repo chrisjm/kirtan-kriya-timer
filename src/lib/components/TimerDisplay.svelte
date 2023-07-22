5490610bd19a7dadcceffde7bc80ab34c07d0801
Chris J Mears <chris.mears@gmail.com>
2023-07-22 22:38:30 +0000

    Add resource links

--- src/lib/components/TimerDisplay.svelte
+++ src/lib/components/TimerDisplay.svelte
@@ -223,3 +223,31 @@
 		{/if}
 	</div>
 </section>
+<section class="my-6">
+	<h2 class="text-lg font-medium">Resources</h2>
+	<ul class="list-disc pl-6">
+		<li>
+			<a
+				href="https://alzheimersprevention.org/research/kirtan-kriya-yoga-exercise/"
+				class="link link-primary"
+				target="_blank"
+				>Practice The 12-Minute Yoga Meditation Exercise - Alzheimer's Research and Prevention
+				Foundation</a
+			>
+		</li>
+		<li>
+			<a
+				href="https://www.ijhsr.org/IJHSR_Vol.11_Issue.1_Jan2021/IJHSR35.pdf"
+				class="link link-primary"
+				target="_blank"
+				>A Review on Therapeutic Effect of Kirtan Kriya Yoga - International Journal of Health
+				Sciences and Research</a
+			>
+		</li>
+		<li>
+			<a href="https://youtu.be/hHFMxq2wjR4" class="link link-primary" target="_blank"
+				>Dr. Dharma on the Healing Power of Kirtan Kriya - YouTube</a
+			>
+		</li>
+	</ul>
+</section>
