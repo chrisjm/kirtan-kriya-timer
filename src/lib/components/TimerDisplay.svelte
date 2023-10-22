643c93844aca4e69f92d7d40abb322773155c8ce
Chris J Mears <chris.mears@gmail.com>
2023-10-22 11:26:59 -0700

    Comment out sound code

--- src/lib/components/TimerDisplay.svelte
+++ src/lib/components/TimerDisplay.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import * as Tone from 'tone';
+	// import * as Tone from 'tone';
 
 	const millisecondsToSeconds: (milliseconds: number) => number = (milliseconds) =>
 		Math.round(milliseconds / 1000);
@@ -13,9 +13,9 @@
 	}
 
 	// TODO: This should be more general and probably linear interpolation?
-	const volumeToDecibels = (value: number, r1 = [0, 100], r2 = [-48, 0]) => {
-		return Math.floor(((value - r1[0]) * (r2[1] - r2[0])) / (r1[1] - r1[0]) + r2[0]);
-	};
+	// const volumeToDecibels = (value: number, r1 = [0, 100], r2 = [-48, 0]) => {
+	// 	return Math.floor(((value - r1[0]) * (r2[1] - r2[0])) / (r1[1] - r1[0]) + r2[0]);
+	// };
 
 	let isRunning = false;
 	let actions = [
@@ -32,49 +32,49 @@
 		minutesToMilliseconds(2),
 		minutesToMilliseconds(2)
 	];
-	let volumes = [90, 60, 30, 60, 90];
+	// let volumes = [90, 60, 30, 60, 90];
 	let timerEndTime: number;
 	let timerId: NodeJS.Timer;
 	let timerIndex = 0;
 	let timeRemaining: number = durations[0];
 	let mantra = '';
-	let synth: Tone.Synth<Tone.SynthOptions> | Tone.PolySynth<Tone.Synth<Tone.SynthOptions>>;
-	let loop: Tone.Loop<Tone.LoopOptions>;
-	let vol: Tone.Volume;
-	let noteIndex = 0;
-	let soundLoaded = false;
-	let soundEnabled = false;
-	let soundPlaying = false;
+	// let synth: Tone.Synth<Tone.SynthOptions> | Tone.PolySynth<Tone.Synth<Tone.SynthOptions>>;
+	// let loop: Tone.Loop<Tone.LoopOptions>;
+	// let vol: Tone.Volume;
+	// let noteIndex = 0;
+	// let soundLoaded = false;
+	// let soundEnabled = false;
+	// let soundPlaying = false;
 	let duration = durations[timerIndex];
 
 	$: actionLabel = actions[timerIndex];
-	$: volumeValue = volumes[timerIndex];
+	// $: volumeValue = volumes[timerIndex];
 
-	$: {
-		if (synth && volumeValue) {
-			synth.volume.value = volumeToDecibels(+volumeValue);
-		}
-	}
+	// $: {
+	// 	if (synth && volumeValue) {
+	// 		synth.volume.value = volumeToDecibels(+volumeValue);
+	// 	}
+	// }
 
-	$: {
-		if (soundPlaying && soundEnabled) {
-			startTones();
-			if (vol) {
-				vol.mute = false;
-			}
-		} else {
-			if (vol) {
-				vol.mute = true;
-			}
-		}
-	}
+	// $: {
+	// 	if (soundPlaying && soundEnabled) {
+	// 		startTones();
+	// 		if (vol) {
+	// 			vol.mute = false;
+	// 		}
+	// 	} else {
+	// 		if (vol) {
+	// 			vol.mute = true;
+	// 		}
+	// 	}
+	// }
 
-	const notes = [
-		{ pitch: 'E3', mantra: 'Sa' },
-		{ pitch: 'D3', mantra: 'Ta' },
-		{ pitch: 'C3', mantra: 'Na' },
-		{ pitch: 'D3', mantra: 'Ma' }
-	];
+	// const notes = [
+	// 	{ pitch: 'E3', mantra: 'Sa' },
+	// 	{ pitch: 'D3', mantra: 'Ta' },
+	// 	{ pitch: 'C3', mantra: 'Na' },
+	// 	{ pitch: 'D3', mantra: 'Ma' }
+	// ];
 
 	function updateCountdown() {
 		clearTimeout(timerId);
@@ -96,11 +96,11 @@
 
 	async function startTimers() {
 		isRunning = true;
-		soundPlaying = true;
+		// soundPlaying = true;
 		timerEndTime = Date.now() + timeRemaining;
-		if (!soundLoaded) {
-			initializeSound();
-		}
+		// if (!soundLoaded) {
+		// 	initializeSound();
+		// }
 		updateCountdown();
 	}
 
@@ -113,43 +113,43 @@
 
 	function resetTimers() {
 		timerIndex = 0;
-		noteIndex = 0;
+		// noteIndex = 0;
 		timeRemaining = durations[timerIndex];
-		stopTones();
+		// stopTones();
 	}
 
 	function pauseTimers() {
 		clearTimeout(timerId);
 		isRunning = false;
-		Tone.Transport.pause();
+		// Tone.Transport.pause();
 	}
 
-	function stopTones() {
-		soundPlaying = false;
-		Tone.Transport.stop();
-	}
+	// function stopTones() {
+	// 	soundPlaying = false;
+	// 	Tone.Transport.stop();
+	// }
 
-	function startTones() {
-		soundPlaying = true;
-		Tone.Transport.bpm.value = 60;
-		Tone.Transport.start();
-	}
+	// function startTones() {
+	// 	soundPlaying = true;
+	// 	Tone.Transport.bpm.value = 60;
+	// 	Tone.Transport.start();
+	// }
 
 	// Must be initialized via user interation because of the audio
-	async function initializeSound() {
-		await Tone.start();
-		vol = new Tone.Volume(volumeToDecibels(volumeValue)).toDestination();
-		synth = new Tone.Synth().connect(vol);
-		loop = new Tone.Loop((time) => {
-			synth.triggerAttackRelease(notes[noteIndex].pitch, '4n', time);
-			mantra = notes[noteIndex].mantra;
-			noteIndex += 1;
-			if (noteIndex >= notes.length) {
-				noteIndex = 0;
-			}
-		}).start(0);
-		soundLoaded = true;
-	}
+	// async function initializeSound() {
+	// 	await Tone.start();
+	// 	vol = new Tone.Volume(volumeToDecibels(volumeValue)).toDestination();
+	// 	synth = new Tone.Synth().connect(vol);
+	// 	loop = new Tone.Loop((time) => {
+	// 		synth.triggerAttackRelease(notes[noteIndex].pitch, '4n', time);
+	// 		mantra = notes[noteIndex].mantra;
+	// 		noteIndex += 1;
+	// 		if (noteIndex >= notes.length) {
+	// 			noteIndex = 0;
+	// 		}
+	// 	}).start(0);
+	// 	soundLoaded = true;
+	// }
 
 	function handleTimerSelect(index: number) {
 		if (isRunning) {
@@ -159,10 +159,10 @@
 		timeRemaining = durations[index];
 	}
 
-	function handleSoundToggle() {
-		soundPlaying = !soundPlaying;
-		console.log(soundPlaying);
-	}
+	// function handleSoundToggle() {
+	// 	soundPlaying = !soundPlaying;
+	// 	console.log(soundPlaying);
+	// }
 </script>
 
 <section class="text-center my-6">
@@ -205,7 +205,7 @@
 		{/each}
 	</ul>
 </section>
-<section class="my-6">
+<!-- <section class="my-6">
 	<div class="flex gap-3">
 		<label class="cursor-pointer label flex flex-col gap-2">
 			<span class="label-text">Sound</span>
@@ -223,7 +223,7 @@
 			</label>
 		{/if}
 	</div>
-</section>
+</section> -->
 <section class="my-6">
 	<div tabindex="-1" class="collapse collapse-arrow border border-base-300 bg-base-200">
 		<div class="collapse-title text-xl font-medium">Resources</div>
